1f9a59799be4b8ca85140dab8c96bb342eeb407a
alexis stephan <alexis-stephan@hotmail.fr>
2024-02-26 10:18:06 +0100

    rails new + install devise
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters_update, if: :devise_controller?
+  before_action :authenticate_user!
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :picture])
+  end
+  def configure_permitted_parameters_update
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
